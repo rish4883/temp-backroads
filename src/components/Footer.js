25044d142b884344d799dadbbb2a836e6374694b
rishabh <rishabh4883@gmail.com>
2023-09-18 07:39:03 +0530

    chaged

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,7 +19,12 @@ const Footer = () => {
         {socialLinks.map((link) => {
           return (
             <li key={link.id}>
-              <a href={link.href} target="_blank" className="footer-icon">
+              <a
+                href={link.href}
+                target="_blank"
+                className="footer-icon"
+                rel="noreferrer"
+              >
                 <i className={link.icon}></i>
               </a>
             </li>
